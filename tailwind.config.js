cc5754a513d200c66801e9df237cbaed0d028479
Arush Nandakumar <131232484+arushn47@users.noreply.github.com>
2025-07-26 00:42:33 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'primary-orange': '#FF5722', // Example hex code for the orange color
+      },
+    },
+  },
+  plugins: [],
+}
